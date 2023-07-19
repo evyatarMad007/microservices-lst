4df893dbf3efd11f394c73f4f791cb392b45f163
= <=>
2023-07-19 19:31:12 +0300

    done

--- event-bus/index.js
+++ event-bus/index.js
@@ -1,21 +1,26 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const axios = require("axios");
-const { sendEvent } = require("./utils/event-lost-handler");
 
 const app = express();
 app.use(bodyParser.json());
 
+const events = [];
 
 app.post("/events", async (req, res) => {
     const event = req.body;
+    events.push(event);
   
-    sendEvent("http://localhost:4000/events", event)
-    sendEvent("http://localhost:4001/events", event)
-    sendEvent("http://localhost:4002/events", event)
-    sendEvent("http://localhost:4003/events", event)
+    axios.post("http://localhost:4000/events", event).catch((err) => {});
+    axios.post("http://localhost:4001/events", event).catch((err) => {});
+    axios.post("http://localhost:4002/events", event).catch((err) => {});
+    axios.post("http://localhost:4003/events", event).catch((err) => {});
 
     res.send({ status: "OK" });
+});
+
+app.get("/events", (req, res) => {
+    res.send(events);
 });
   
 
